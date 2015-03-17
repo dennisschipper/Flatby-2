dfc6ea808e44e4b27524f36d561457d072c45ea8
Dennis Schipper <dennis@designfordetails.com>
2015-03-17 23:23:24 +0000

    some modal code

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
   console.log("hey");
+
+  $('.toggle-modal').click(function() {
+    // $('.modal').toggle();
+    // $('.cover').toggle();
+  })
+
+  $('.cover').click(function() {
+    $('.modal').addClass('minimized');
+    // $('.modal').toggle();
+    // $('.cover').toggle();
+  })
+
 })
